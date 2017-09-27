f2a5bd2ffcec0efd1e1026d2c4f437e07972b45b
Jibran Kalia <jkalia@student.42.us.org>
2017-09-26 19:40:31 -0700

    serving static html

--- api/routes.js
+++ api/routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
-
 const boutique = require('./project.js');
 
 router.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../index.html`));
   boutique.callApi()
-    .then(out => console.log(out))
-    .then(res.send('done'));
+    .then(out => console.log(out));
 });
 
 module.exports = router;
