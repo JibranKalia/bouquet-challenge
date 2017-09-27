d5c45fa722e6cf03ceafcbfef4c36086b16e3a92
Jibran Kalia <jkalia@student.42.us.org>
2017-09-26 19:49:54 -0700

    Configuring heroku

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
 app.use(require('./api/routes.js'));
 
-app.listen(port, () => {
-  console.log(`We are live on ${port}`);
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+    
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
+
